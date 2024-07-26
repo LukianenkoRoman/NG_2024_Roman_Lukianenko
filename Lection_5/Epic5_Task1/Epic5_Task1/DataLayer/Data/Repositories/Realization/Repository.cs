a071c0ec1582fc7616e63e1415af72dbcfd6766d
LukianenkoRoman <roma-03@ukr.net>
2024-07-26 13:34:02 +0300

    Repository realization class was added

---
+++ Lection_5/Epic5_Task1/Epic5_Task1/DataLayer/Data/Repositories/Realization/Repository.cs
@@ -0,0 +1,55 @@
+﻿using DataLayer.Entities;
+using Epic5_Task1.DataLayer.Data.Repositories.Interfaces;
+using Epic5_Task1.DataLayer.Data.Infrastucture;
+using Epic5_Task1.DataLayer.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Epic5_Task1.DataLayer.Data.Repositories.Realization
+{
+    public class Repository : IRepository<IEntity> 
+    {
+        private readonly Task5DbContext _dbContext;
+
+        public Repository(Task5DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEntity> Create(IEntity entity)
+        {
+            var createdEntity = await _dbContext.Set<IEntity>().AddAsync(entity);
+            return createdEntity.Entity;
+        }
+
+        public async Task Delete(Guid id)
+        {
+            var entity = _dbContext.Set<IEntity>().Find(id);
+            if (entity != null)
+            {
+                _dbContext.Set<IEntity>().Remove(entity);
+            }
+        }
+
+        public async Task<IEntity> Find(Guid id)
+        {
+            return await _dbContext.Set<IEntity>().FindAsync(id);
+        }
+
+        public IQueryable<IEntity> GetAll()
+        {
+            return _dbContext.Set<IEntity>();
+        }
+
+        public async Task<List<IEntity>> GetAllAsync(Expression<Func<IEntity, bool>> expression)
+        {
+            return await _dbContext.Set<IEntity>().Where(expression).ToListAsync();
+        }
+
+        public async Task<IEntity> Update(IEntity entity)
+        {
+            var updatedEntity = _dbContext.Set<IEntity>().Update(entity);
+            return updatedEntity.Entity;
+        }
+    }
+}
