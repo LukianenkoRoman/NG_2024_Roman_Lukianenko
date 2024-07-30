33d7bd6dcc0e9119c6fb4257809442c7d85d10a7
LukianenkoRoman <roma-03@ukr.net>
2024-07-30 14:21:22 +0300

    UoF interface and file was created

---
+++ Lection_5/Epic5_Task1/Epic5_Task1/DataLayer/Data/Infrastucture/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using DataLayer.Entities;
+using Epic5_Task1.DataLayer.Data.Repositories.Interfaces;
+
+
+namespace Epic5_Task1.DataLayer.Data.Infrastucture
+{
+    public interface IUnitOfWork
+    {
+        ICategoryRepository CategoryRepository { get; }
+        IClientRepository ClientRepository { get; }
+        IEmployerRepository EmployerRepository { get; }
+        IItemRepository ItemRepository { get; }
+        IManagerRepository ManagerRepository { get; }
+        IStorageRepository StorageRepository { get; }
+
+        Task SaveChangesAsync();
+    }
+}
